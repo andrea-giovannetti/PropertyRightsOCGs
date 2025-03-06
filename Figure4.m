670075726b65ff0bf5c8e6e7433c0ab41828ab68
andrea-giovannetti <162666339+andrea-giovannetti@users.noreply.github.com>
2025-03-06 17:37:54 +1100

    Update and rename simu_wrapper.m to Figure4.m

--- Figure4.m
+++ Figure4.m
@@ -112,4 +112,4 @@ hold off
 fig.PaperPositionMode = 'auto';
 fig_pos = fig.PaperPosition;
 fig.PaperSize = [fig_pos(3) fig_pos(4)];
-print(fig,'c://Users/giova/Dropbox/Sydney/ACU/Research/Property Rights/fights_10_disaggregated_fig4','-dpdf')
+print(fig,'fights_10_disaggregated_fig4','-dpdf')
